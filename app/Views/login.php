48e9f7307f3877cfe7511f58831f2e77a6af2e1e
IDOUFKIR Kamal <idoufkir.kamal@gmail.com>
2024-12-28 21:38:09 +0100

    remove logincontroller

--- app/Views/login.php
+++ app/Views/login.php
@@ -32,51 +32,51 @@
                             <label class="block text-sm font-medium text-gray-600 dark:text-gray-200 mb-2" for="LoggingEmailAddress">Email Address</label>
                             <input id="LoggingEmailAddress" class="form-input" type="email" placeholder="Enter email" >
                         </div>
-
-                        <div class="mb-4">
-                            <label class="block text-sm font-medium text-gray-600 dark:text-gray-200 mb-2" for="loggingPassword">Password</label>
-                            <input id="loggingPassword" class="form-input" type="password" placeholder="Enter password" >
-                        </div>
-
-                        <div class="flex items-center justify-between mb-4">
-                            <div class="flex items-center">
-                                <input type="checkbox" class="form-checkbox rounded" id="checkbox-signin">
-                                <label class="ms-2" for="checkbox-signin">Remember me</label>
+                        <form method="post" action="<?= base_url('login') ?>">
+                            <div class="mb-4">
+                                <label class="block text-sm font-medium text-gray-600 dark:text-gray-200 mb-2" for="loggingPassword">Password</label>
+                                <input id="loggingPassword" class="form-input" type="password" placeholder="Enter password" >
                             </div>
-                            <a href="auth-recoverpw.html" class="text-sm text-primary border-b border-dashed border-primary">Forget Password ?</a>
-                        </div>
 
-                        <div class="flex justify-center mb-6">
-                            <button class="btn w-full text-white bg-primary"> Log In </button>
-                        </div>
+                            <div class="flex items-center justify-between mb-4">
+                                <div class="flex items-center">
+                                    <input type="checkbox" class="form-checkbox rounded" id="checkbox-signin">
+                                    <label class="ms-2" for="checkbox-signin">Remember me</label>
+                                </div>
+                                <a href="auth-recoverpw.html" class="text-sm text-primary border-b border-dashed border-primary">Forget Password ?</a>
+                            </div>
 
-                        <div class="flex items-center my-6">
-                            <div class="flex-auto mt-px border-t border-dashed border-gray-200 dark:border-slate-700"></div>
-                            <div class="mx-4 text-secondary">Or</div>
-                            <div class="flex-auto mt-px border-t border-dashed border-gray-200 dark:border-slate-700"></div>
-                        </div>
+                            <div class="flex justify-center mb-6">
+                                <button class="btn w-full text-white bg-primary"> Log In </button>
+                            </div>
 
-                        <div class="flex gap-4 justify-center mb-6">
-                            <a href="javascript:void(0)" class="btn border-light text-gray-400 dark:border-slate-700">
-                                <span class="flex justify-center items-center gap-2">
-                                    <i class="mgc_github_line text-info text-xl"></i>
-                                    <span class="lg:block hidden">Github</span>
-                                </span>
-                            </a>
-                            <a href="javascript:void(0)" class="btn border-light text-gray-400 dark:border-slate-700">
-                                <span class="flex justify-center items-center gap-2">
-                                    <i class="mgc_google_line text-danger text-xl"></i>
-                                    <span class="lg:block hidden">Google</span>
-                                </span>
-                            </a>
-                            <a href="javascript:void(0)" class="btn border-light text-gray-400 dark:border-slate-700">
-                                <span class="flex justify-center items-center gap-2">
-                                    <i class="mgc_facebook_line text-primary text-xl"></i>
-                                    <span class="lg:block hidden">Facebook</span>
-                                </span>
-                            </a>
-                        </div>
+                            <div class="flex items-center my-6">
+                                <div class="flex-auto mt-px border-t border-dashed border-gray-200 dark:border-slate-700"></div>
+                                <div class="mx-4 text-secondary">Or</div>
+                                <div class="flex-auto mt-px border-t border-dashed border-gray-200 dark:border-slate-700"></div>
+                            </div>
 
+                            <div class="flex gap-4 justify-center mb-6">
+                                <a href="javascript:void(0)" class="btn border-light text-gray-400 dark:border-slate-700">
+                                    <span class="flex justify-center items-center gap-2">
+                                        <i class="mgc_github_line text-info text-xl"></i>
+                                        <span class="lg:block hidden">Github</span>
+                                    </span>
+                                </a>
+                                <a href="javascript:void(0)" class="btn border-light text-gray-400 dark:border-slate-700">
+                                    <span class="flex justify-center items-center gap-2">
+                                        <i class="mgc_google_line text-danger text-xl"></i>
+                                        <span class="lg:block hidden">Google</span>
+                                    </span>
+                                </a>
+                                <a href="javascript:void(0)" class="btn border-light text-gray-400 dark:border-slate-700">
+                                    <span class="flex justify-center items-center gap-2">
+                                        <i class="mgc_facebook_line text-primary text-xl"></i>
+                                        <span class="lg:block hidden">Facebook</span>
+                                    </span>
+                                </a>
+                            </div>
+                        </form>
                         <p class="text-gray-500 dark:text-gray-400 text-center">Don't have an account ?<a href="auth-register.html" class="text-primary ms-1"><b>Register</b></a></p>
                     </div>
                 </div>
